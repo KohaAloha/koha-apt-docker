5954673d784ea42cfbf2587d8567a7e4f56a78cb
Mason James <mtj@kohaaloha.com>
2020-01-28 04:25:25 +1300

    modified:   files/run.sh

--- files/run.sh
+++ files/run.sh
@@ -12,19 +12,8 @@ export TEMP=/tmp
 # dpkg -l | grep openapi
 # dpkg -l | grep validator
 
-if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
-
     if apt install -y koha-common1; then
-#        touch testing.success
         continue;
     else
-        exit 2;
+        echo "TESTS FAILED"
     fi
-else
-    exit 2;
-
-    # TODO: We could use supervise as the main loop
-#    /bin/bash -c "trap : TERM INT; sleep infinity & wait"
-
-fi
-
