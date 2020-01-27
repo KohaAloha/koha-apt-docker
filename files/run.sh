3c562c8ea1130d20744b0cc48f7657c6d874cfec
Mason James <mtj@kohaaloha.com>
2020-01-28 02:22:19 +1300

    modified:   files/run.sh

--- files/run.sh
+++ files/run.sh
@@ -9,6 +9,7 @@ export TEMP=/tmp
 # while ! nc -z db 3306; do sleep 1; done
 
 # dpkg -l
+
 dpkg -l | grep mojo
 dpkg -l | grep openapi
 dpkg -l | grep validator
@@ -16,16 +17,9 @@ dpkg -l | grep validator
 echo '7777777777777777777777777777777777777777777777'
 if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
 
-    cd ${BUILD_DIR}/koha
-    rm -rf /cover_db/*
-
     if  apt install -s koha-common ; then
         touch testing.success;
-        echo command returned true;
     else
-        echo command returned some error
-    fi
-
 
 else
     # TODO: We could use supervise as the main loop
